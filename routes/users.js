dd6a9d365d50faaa4695d8f767040c748957baed
Goat King <joaki2003@gmail.com>
2025-04-02 22:46:08 +0200

    Started player 1 submit

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/player1', function(req, res, next) {
+  res.render('player1');
+});
+
+router.get('/player2', function(req, res, next) {
+  res.render('player2');
+});
+
 module.exports = router;

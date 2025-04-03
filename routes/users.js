26b96f7254c19c21d90fc2a05be0f44e56e8199f
Goat King <joaki2003@gmail.com>
2025-04-04 00:10:43 +0200

    P1 can input a passcode
    Player 1 can input a code which is saved as a variable we can check the value of.

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,24 @@ router.get('/player1', function(req, res, next) {
   res.render('player1');
 });
 
+router.get('/congrats', function(req, res, next) {
+  res.render('congrats');
+});
+
+const rightCode = "banana";
+router.post('/player1', function(req, res, next) {
+  console.log(req.body.codeGuess);
+  if(req.body.codeGuess == rightCode){
+    res.redirect("/users/congrats");
+  }
+  else{
+    console.log("Wrong code");
+    res.render("player1", { prevGuess: req.body.codeGuess });
+  }
+
+
+});
+
 router.get('/player2', function(req, res, next) {
   res.render('player2');
 });

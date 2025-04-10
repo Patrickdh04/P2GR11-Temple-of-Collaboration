95f6895a61dab52780f03dea9662a7b1ee1db408
NikolajAAU <eo81wb@student.aau.dk>
2025-04-10 20:10:23 +0200

    Imported functions to puzzle 2 and added a new ropes array that has 5 ropes each with a rule and color.

--- routes/puzzle2.js
+++ routes/puzzle2.js
@@ -3,21 +3,17 @@ var router = express.Router();
 
 const ropeColor = ["Yellow", "Blue", "Green", "Orange", "Purple", "Red", "Cyan", "Black"];
 const rules = ["First", "Striped", "Half white", "Not straight", "Longest"];
-
-// import * as functions from "./functions.js";
-
-function shuffle(array) {
-    let oldElement;
-    for (let i = array.length - 1; i > 0; i--) {
-        let rand = Math.floor(Math.random() * (i + 1));
-        oldElement = array[i];
-        array[i] = array[rand];
-        array[rand] = oldElement;
-    }
-    return array;
-}
-//Test
-shuffle(ropeColor);
+const {shuffleArray} = require('./functions');
+const ropes = [5];
+shuffleArray(ropeColor);
 console.log(ropeColor);
+console.log(rules);
 
+for (let i = 0; i < 5; i++){
+    ropes.push ({
+        color: ropeColor[i], rule: rules[i]
+    }) ;
+}
+
+console.log(ropes[1]);
 module.exports = router;

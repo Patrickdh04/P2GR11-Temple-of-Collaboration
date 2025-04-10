078778a84284974258b6fc709a3a1afa5fdbf959
NikolajAAU <eo81wb@student.aau.dk>
2025-04-10 12:28:32 +0200

    Update puzzle2.js

--- routes/puzzle2.js
+++ routes/puzzle2.js
@@ -1,5 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+const ropeColor = ["Yellow", "Blue", "Green", "Orange", "Purple", "Red", "Cyan", "Black"];
+const rules = ["First", "Striped", "Half white", "Not straight", "Longest"];
+
+// import * as functions from "./functions.js";
+
+function shuffle(array) {
+    let oldElement;
+    for (let i = array.length - 1; i > 0; i--) {
+        let rand = Math.floor(Math.random() * (i + 1));
+        oldElement = array[i];
+        array[i] = array[rand];
+        array[rand] = oldElement;
+    }
+    return array;
+}
+
+shuffle(ropeColor);
+console.log(ropeColor);
 
 module.exports = router;

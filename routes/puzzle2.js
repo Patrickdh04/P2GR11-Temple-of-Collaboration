a566018965b599c80f810ce5fc1b12f20b499d9a
NikolajAAU <eo81wb@student.aau.dk>
2025-04-13 01:49:45 +0200

    Added Nodemon locally and router.get for room 2

--- routes/puzzle2.js
+++ routes/puzzle2.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const {shuffleArray} = require('./functions');
 
 const ropeColor = ["Yellow", "Blue", "Green", "Orange", "Purple", "Red", "Cyan", "Black"];
 const rules = ["First", "Striped", "Half white", "Not straight", "Longest"];
-const {shuffleArray} = require('./functions');
 const ropes = [5];
 shuffleArray(ropeColor);
 console.log(ropeColor);
@@ -16,4 +16,10 @@ for (let i = 0; i < 5; i++){
 }
 
 console.log(ropes[1]);
+
+
+
+router.get('/puz2_player1', function (req, res, next) {
+    res.render('puz2_player1');
+});
 module.exports = router;

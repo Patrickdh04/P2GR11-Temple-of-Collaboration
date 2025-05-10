c72b522a103a3ad01ff435d3e097f4a215a9e38a
NikolajAAU <eo81wb@student.aau.dk>
2025-05-10 18:15:52 +0200

    Downloaded .env plugin and set up and connected to Atlas

--- routes/roomDB.js
+++ routes/roomDB.js
@@ -2,4 +2,3 @@ const express = require('express');
 const router = express.Router();
 const Room = require('../../models/room');
 
-//Next up: Set up Atlas and change MongoDBURI with the one now created in Atlas

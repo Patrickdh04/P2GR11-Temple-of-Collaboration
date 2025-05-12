60c27e7eba4bb0294c88c7a8776b2dda6f428f9a
GabsBlake <Gabrielluurblake@outlook.com>
2025-05-12 13:01:41 +0200

    puzzle 2 player 2 clues respond to pullsequence
    Fixed the display of the pullsequence clues

--- routes/puzzle2.js
+++ routes/puzzle2.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const {shuffleArray} = require('./functions');
 
-const ropeColor = ["Yellow", "Blue", "Green", "Orange", "Purple", "Red", "Cyan", "Black"];
+const ropeColour = ["Yellow", "Blue", "Green", "Orange", "Purple", "Red", "Cyan", "Black"];
 const rules = ["First", "Striped", "Half white", "Wavy", "Distorted"];
 const ruleToFolder ={
     "First": "solid",
@@ -12,16 +12,16 @@ const ruleToFolder ={
     "Distorted": "distorted"
 };
 const allRopes = [];
-shuffleArray(ropeColor);
+shuffleArray(ropeColour);
 shuffleArray(rules);
-console.log(ropeColor);
+/*console.log(ropeColour);
 console.log(rules);
-let test = 0;
+const test = 0; */
 
 // Building rope objects
 for (let i = 0; i < 5; i++){
     allRopes.push ({
-        color: ropeColor[i], rule: rules[i], folder: ruleToFolder[rules[i]]
+        colour: ropeColour[i], rule: rules[i], folder: ruleToFolder[rules[i]]
     }) ;
 }
 
@@ -37,114 +37,77 @@ if (0 !== firstRopeIndex) {
 
 // The sequence in which the ropes should be clicked/pulled
 
+
 const pullSequence = displayRopes.map((_, i) => i);
+
+
 shuffleArray(pullSequence);
 
 console.log(displayRopes);
+console.log (pullSequence)
 
-// Functions to analyse rope layout
+buildCorrectParagraph(displayRopes, pullSequence);
 
-function countColor(ropes, color){
-    return ropes.filter(r => r.color === color).length;
-}
 
-function hasRule(ropes, rule) {
-    return ropes.some(r => r.rule === rule);
-}
+// Generating correct paragraph
+function buildCorrectParagraph(displayRopes, pullSequence) {
+    const tempClues = [];
+    
+    for (let i = 0; i < pullSequence.length; i++) {
+        const ropeIndex = pullSequence[i];
+        const rope = displayRopes[ropeIndex];
 
-function colourAt(ropes, index){
-    return ropes[index]?.color
-}
-
-function ruleAt(ropes, index){
-    return ropes[index]?.rule;
-}
-
-function buildClue(condition, action) {
-    return `if ${condition}, pull the ${action}.`;
-}
-
-//Generating correct paragraph for manual
-
-function buildCorrectParagraph(ropes){
-
-    const clues =[];
-
-    const redCount = countColor(ropes, "Red");
-    const hasDistorted = hasRule(ropes, "Distorted");
-
-    const secondColour = colourAt(ropes, 1);
-    const thirdRule = ruleAt(ropes, 2);
-    const fourthColour = colourAt(ropes, 3);
-    const fifthRule = ruleAt(ropes, 4);
-
-  
-    let redCondition = "";
-    if (redCount === 0) {
-        redCondition = "there are no red ropes";
-    } else if (redCount === 1) {
-        redCondition = "there is exactly one red rope";
-    } else if (redCount === 2) {
-        redCondition = "there are exactly two red ropes";
-    } else {
-        redCondition = "there are more than two red ropes";
+        tempClues.push(`The ${ordinal(i + 1)} rope to pull is the ${rope.rule.toLowerCase()} ${rope.colour.toLowerCase()} rope.`);
     }
 
-      clues.push(buildClue(redCondition, "third rope"));
-      clues.push(buildClue(`the second rope is ${secondColour.toLowerCase()}`,"the last rope"));
-      clues.push(buildClue(`the third rope has the pattern ${thirdRule.toLowerCase()}`,"the second rope"));
-      clues.push(buildClue(`the fourth rope is ${fourthColour.toLowerCase()}`,"the first rope"));
-      clues.push(buildClue(`the fifth rope has the pattern ${fifthRule.toLowerCase()}`,"the sixth rope"));
-      clues.push(buildClue(hasDistorted ? "at least one rope is distorted": "none of the ropes are distorted","the fourth rope"));
-      
-      return clues;
-}
+    const clues = [5]
+
+    for (let index = 0; index < pullSequence.length; index++) {
+        clues[index] = tempClues[pullSequence[index]]
+    }
+
+    return clues;
+    console.log(clues);
+
+    // Helper to convert 1 to 1st etc.
+    function ordinal(n) {
+        const suffixes = ["th", "st", "nd", "rd"];
+        const v = n % 100;
+        return n + (suffixes[(v - 20) % 10] || suffixes[v] || suffixes[0]);
+    }}
 
 // helper function
 function getRandomElement(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
 }
 
+function buildClue(condition, action) {
+    return `if ${condition}, pull the ${action}.`;}
 
 // Generating 3 decoy paragraphs for manual
 
 function buildDecoyParagraphs(ropes, count = 3) {
-    const allColors = ["Red", "Blue", "Green", "Orange", "Purple", "Yellow", "Black", "Cyan"];
-    const allRules = ["Distorted", "Half-white", "Solid", "Striped", "Wavy", "First"];
+    const allColours = ["Red", "Pink", "Silver", "Brass", "Bronze", "Gold", "White", "Clear"];
+    const allRules = ["Straight", "Half-brown", "Solid", "Polkadotted", "Zig-zagged", "Second"];
     const allPositions = ["first", "second", "third", "fourth", "fifth", "sixth"];
-    const allActions = ["first rope", "second rope", "third rope", "fourth rope","fifth rope", "last rope", "the rope before the red one"];
+    const allActions = ["first rope", "second rope", "third rope", "fourth rope","fifth rope", "last rope", "the rope before the last one"];
 
     const paragraphs = [];
 
     for (let i = 0; i < count; i++) {
         const clues = [];
 
-        // red condition
-        const fakeRedCount = Math.floor(Math.random() * 5);
-        let redCondition = "";
-        if (fakeRedCount === 0) {
-            redCondition = "there are no red ropes";
-        } else if (fakeRedCount === 1) {
-            redCondition = "there is exactly one red rope";
-        } else if (fakeRedCount === 2) {
-            redCondition = "there are exactly two red ropes";
-        } else {
-            redCondition = "there are more than two red ropes";
-        }
-
-        clues.push(buildClue(redCondition, getRandomElement(allActions)));
-
         // rest of the clues
         for (let j = 0; j < 4; j++) {
-            const color = getRandomElement(allColors).toLowerCase();
+            const colour = getRandomElement(allColours).toLowerCase();
             const rule = getRandomElement(allRules).toLowerCase();
             const position = getRandomElement(allPositions);
             const action = getRandomElement(allActions);
 
             if (Math.random() < 0.5) {
-                clues.push(buildClue(`the ${position} rope is ${color}`, action));
+                clues.push(buildClue(`the ${position} rope is ${colour}`, action));
             } else {
-                clues.push(buildClue(`the \${position} rope has the pattern ${rule}`, action));
+                clues.push(buildClue(`the ${position} rope has the pattern ${rule}`, action));
             }
         }
 
@@ -159,14 +122,16 @@ function buildDecoyParagraphs(ropes, count = 3) {
 
       
 router.get('/puz2_player1', function (req, res, next) {
-    res.render('puz2_player1', {displayRopes, pullSequence, test});
+    res.render('puz2_player1', {displayRopes, pullSequence});
 });
 
 router.get('/puz2_player2', function (req, res, next) {
-    const correctParagraph = buildCorrectParagraph(displayRopes);
+    const correctParagraph = buildCorrectParagraph(displayRopes, pullSequence);
     const decoyParagraphs = buildDecoyParagraphs(displayRopes, 3);
     const ruleParagraphs = [correctParagraph, ...decoyParagraphs];
     shuffleArray(ruleParagraphs);
+
+    console.log("ruleParagraphs:", ruleParagraphs);
 
     res.render('puz2_player2', {ruleParagraphs});
 });
